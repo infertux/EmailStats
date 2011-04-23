9aa94ce80bc95c8ec5d0055c931028c33694662a
Infertux <infertux@infertux.com>
2011-04-23 23:54:47 +0200

    using now 'References:' header to find threads

--- EmailStats.py
+++ EmailStats.py
@@ -22,6 +22,7 @@ if version_info[0] < 3:
 
 import imaplib, re, collections
 from email.header import decode_header
+from email.parser import HeaderParser
 from math import log10
 
 
@@ -32,7 +33,8 @@ class EmailStats:
         if port == None:
             port = 993 if ssl else 143
 
-        self._con = imaplib.IMAP4_SSL(host, port) if ssl else imaplib.IMAP4(host, port)
+        self._con = imaplib.IMAP4_SSL(host, port) if ssl \
+            else imaplib.IMAP4(host, port)
 
     def login(self, username, password):
         try:
@@ -51,22 +53,22 @@ class EmailStats:
         print("Mailboxes:", ", ".join(boxes))
 
     def printQuota(self, mailbox = 'INBOX'):
-        # Select the right mailbox first (in read-only mode)
+        # select the right mailbox first (in read-only mode)
         status, count = self._con.select(mailbox, True)
         count = int(count[0])
         print(count, ' emails in mailbox "', mailbox, '":', sep="")
 
-        # Get quota
+        # get quota
         status, quota = self._con.getquota('user')
         quota = re.search('\(STORAGE (\d+) (\d+)\)', str(quota))
         if quota == None:
             print("Quota unavailaible!", file=sys.stderr)
         else:
-            print("Quota: ", quota.group(1), "/", quota.group(2), "KB (",
+            print("Quota: ", quota.group(1), "/", quota.group(2), " KiB (",
             round(int(quota.group(1)) / int(quota.group(2)) * 100), "% used)",
             sep="")
 
-    def printStats(self, mailboxes = ['INBOX'], subjectFilter='', minAmount=8):
+    def printStats(self, mailboxes = ['INBOX'], subjectFilter='', nElements=8):
         if not isinstance(mailboxes, list):
             raise TypeError("mailboxes parameter must be a list")
 
@@ -74,75 +76,101 @@ class EmailStats:
         for mailbox in mailboxes:
             status, count = self._con.select(mailbox, True)
 
-            # Get all emails
+            # get all emails
             status, emails_ids = self._con.search(None, 'ALL')
-            # Build a comma-separed list
+            # build a comma-separated list
             emails_ids = emails_ids[0].decode().replace(' ', ',')
-            # Fetch all subjects ('PEEK' avoids to mark email as read)
+            # fetch all subjects ('PEEK' avoids to mark email as read)
             status, emails = self._con.fetch(emails_ids,
-                '(UID BODY.PEEK[HEADER.FIELDS (FROM SUBJECT)])')
+                '(UID BODY.PEEK[HEADER.FIELDS ' +
+                '(MESSAGE-ID FROM SENDER SUBJECT REFERENCES)])')
 
-            # Get *only* useful content
+            # get *only* useful content
             messages.extend([email[1] for email in emails
                 if isinstance(email, tuple)])
 
-        # Strip and parse headers
-        parsed_subjects = []
-        parsed_senders = []
-        for message in messages:
-            #print(message)
-            headers = message.decode('utf-8', 'replace').rstrip("\r\n")
-            headers = re.compile("\r\n([a-zA-Z0-9_-]+):"). \
-                split("\r\n" + headers)[1:]
-            #print(headers)
-            # Headers are optional so we init them empty
-            sender = subject = ""
-            for i in range(0, len(headers), 2):
-                header, value = headers[i], headers[i+1].strip()
-                #print(header.upper())
-                if header.upper() == 'FROM':
-                    sender = value
-                elif header.upper() == 'SUBJECT':
-                    subject = value
-                else:
-                    raise ValueError("Unknown header: " + header)
+        msg_ID_subjects = {}
+        msg_ID_counter = collections.Counter()
+        sender_froms = {}
+        sender_counter = collections.Counter()
 
-            # remove "R{e,E}: " to count the initial message in the thread
-            subject = self._decodeHeader(
-                subject.replace('Re: ', '').replace('RE: ', ''))
-            # Filter only matching subjects
-            if not re.match(subjectFilter, subject):
+        for message in messages:
+            parser = HeaderParser()
+            h = parser.parsestr(message.decode('utf-8', 'replace'))
+            # dirty copy because h is read-only
+            headers = { 'Subject': h['Subject'],
+                        'From': h['From'],
+                        'Message-ID': h['Message-ID'],
+                        'References': h['References'],
+                        'Sender': h['Sender'] }
+            del h, parser
+
+            if not headers['From']:
+                print("Skipping uncommunicative message (no 'From' header).")
+                continue
+
+            headers['From'] = self._decodeHeader(headers['From'])
+
+            if not headers['Sender']:
+                headers['Sender'] = headers['From']
+
+            if not headers['Subject']:
+                headers['Subject'] = ''
+
+            headers['Subject'] = self._decodeHeader(headers['Subject'])
+
+            # remove Re and Fwd
+            headers['Subject'] = re.sub(r'^(Re: |Fwd: )+', '',
+                headers['Subject'], 0, re.IGNORECASE)
+
+            # filter only matching subjects
+            if not re.match(subjectFilter, headers['Subject']):
                 #print("Ignored:", subject)
                 continue
 
             #print("Matched:", subject)
-            parsed_subjects.append(subject)
 
-            sender = self._decodeHeader(sender)
-            parsed_senders.append(sender)
+            if headers['References']:
+                # expect the first id is the first email of the thread
+                thread_id = headers['References'].split()[0]
+            else:
+                thread_id = headers['Message-ID']
 
-        subjects = parsed_subjects
-        senders = parsed_senders
+            msg_ID_subjects[thread_id] = msg_ID_subjects.get(thread_id, set())
+            msg_ID_subjects[thread_id].add(headers['Subject'])
+
+            msg_ID_counter.update([thread_id])
+
+            sender_froms[headers['Sender']] = \
+                sender_froms.get(headers['Sender'], set())
+            sender_froms[headers['Sender']].add(headers['From'])
+
+            sender_counter.update([headers['Sender']])
 
         if subjectFilter != "":
             print("Filter:", subjectFilter)
 
-        print(len(subjects), "messages match")
+        total_messages = sum(msg_ID_counter.values())
+        print(total_messages, "messages")
 
-        # Display only most common messages
-        print("\nThe ", minAmount, " biggest trolls:\n",
-              "-" * (int(log10(minAmount)) + 21), sep="")
-        biggestSubjects = collections.Counter(subjects).most_common(minAmount)
-        for subject, amount in biggestSubjects:
-            print(subject, ': ', amount, " messages (",
-            round(float(amount)/len(subjects) * 100), "%)" , sep="")
+        # display only most common messages
+        print("\nThe ", nElements, " biggest trolls:\n",
+              "-" * (int(log10(nElements)) + 21), sep="")
 
-        print("\nThe ", minAmount, " biggest spammers:\n",
-              "-" * (int(log10(minAmount)) + 23), sep="")
-        biggestSenders = collections.Counter(senders).most_common(minAmount)
-        for sender, amount in biggestSenders:
-            print(sender, ': ', amount, " messages (",
-            round(float(amount)/len(senders) * 100), "%)" , sep="")
+        biggest_msg_IDs = msg_ID_counter.most_common(nElements)
+        for msg_ID, amount in biggest_msg_IDs:
+            subjects = '"' + '"\n  aka "'.join(msg_ID_subjects[msg_ID]) + '"'
+            print(subjects, ': ', amount, " messages (",
+            round(float(amount) / total_messages * 100), "%)" , sep="")
+
+        print("\nThe ", nElements, " biggest spammers:\n",
+              "-" * (int(log10(nElements)) + 23), sep="")
+
+        biggest_senders = sender_counter.most_common(nElements)
+        for sender, amount in biggest_senders:
+            froms = ' aka '.join(sender_froms[sender])
+            print(froms, ': ', amount, " messages (",
+            round(float(amount) / total_messages * 100), "%)" , sep="")
 
     def logout(self):
         self._con.close()
@@ -150,7 +178,19 @@ class EmailStats:
 
 
     def _decodeHeader(self, header):
-        header = header.split(': ', 1)[-1]
+        # awful hack because of a Python bug <http://bugs.python.org/issue1079>
+        # probably one of the most horrible hacks I've ever done! :shame:
+        # if you have another solution, please send a patch :)
+        i = 1
+        while re.search('%' * i, header):
+            i += 1
+            if i > 100:
+                raise OverflowError('WTF?!')
+
+        header = re.sub(r'(=\?)(.*)([^\?]{2}\?=)([^ ])',
+            r'\1\2\3 ' + ('%' * i) + ' \4', header)
+
+        # well, let's properly decode this header now...
         decoded_header = decode_header(header)
         header = ""
         for (part,encoding) in decoded_header:
@@ -161,6 +201,10 @@ class EmailStats:
             else:
                 header += part.decode() + " "
 
-        return header.rstrip(" ")
+        header = header.rstrip(" ")
 
+        # awful hack, the end...
+        header = header.replace(' ' + ('%' * i) + ' ', '')
+
+        return header
 
